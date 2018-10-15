adee7be96280d2e50e21c5da2f3605268822ec30
Rstinkho <t.turchinskiy@bk.ru>
2018-10-15 16:54:44 +0800

    Deployment step 2

--- db.js
+++ db.js
@@ -1,11 +1,33 @@
 const pg = require('pg');
+const url = require('url');
 
-const configs = {
-  user: 'taras',
-  host: '127.0.0.1',
-  database: 'tennis',
-  port: 5432
-};
+if( process.env.DATABASE_URL ){
+
+  //we need to take apart the url so we can set the appropriate configs
+
+  const params = url.parse(process.env.DATABASE_URL);
+  const auth = params.auth.split(':');
+
+  //make the configs object
+  var configs = {
+    user: auth[0],
+    password: auth[1],
+    host: params.hostname,
+    port: params.port,
+    database: params.pathname.split('/')[1],
+    ssl: true
+  };
+
+}else{
+
+  //otherwise we are on the local network
+  var configs = {
+      user: 'taras',
+      host: '127.0.0.1',
+      database: 'tennis',
+      port: 5432
+  };
+}
 
 const pool = new pg.Pool(configs);
 
@@ -25,3 +47,7 @@ module.exports = {
     profileModel,
     messageModel
 };
+
+
+//this is the same
+const pool = new pg.Pool(configs);

b54a609a488b3896968d8b532342acec1ae92f03
Rstinkho <t.turchinskiy@bk.ru>
2018-10-15 16:51:50 +0800

    Deployment step 2

--- index.js
+++ index.js
@@ -32,4 +32,6 @@ app.engine('jsx', reactEngine);
 
 routes(app, db);
 
-const server = app.listen(3000, () => console.log('~~~ Tuning in to the waves of port 3000 ~~~'));
+const PORT = process.env.PORT || 3000;
+
+const server = app.listen(PORT, () => console.log('~~~ Tuning in to the waves of port '+PORT+' ~~~'));
